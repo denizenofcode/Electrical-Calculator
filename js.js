8e060501f67baba431fd43198bc096b62b962911
denizenofcode <austin.buie@gmail.com>
2019-01-22 15:53:15 -0500

    Layout
    Added barebones layout

---
+++ js.js
@@ -0,0 +1,24 @@
+/*
+var count = 0;
+var color ="blue";
+
+var measureType = prompt( "Press: 'a' to convert between inches/centimeters.\nPress 'b' to convert between ");
+
+if (measureType == 1) {
+  var userNumber = prompt("Enter your measurement in numeral form.");
+  var switchType = prompt("To convert from inches to centimeters enter 1.  For centimeters to inches enter 2.");
+  if (switchType == 1) {
+    console.log(userNumber * 2.54);
+  } else if (switchType == 2) {
+    console.log(userNumber / 2.54);
+  } else {
+    console.log("You entered an incorrect choice for switchType.");
+  }
+} else {
+    console.log("You didn't enter a correct choice for conversion type.  Please try again.");
+}
+*/
+
+function voltCurResPowCalc() {
+  
+}
